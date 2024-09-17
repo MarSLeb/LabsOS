5822df7e2993ac69ba8dff05ed00b033cd44cf40
Lebedeva Maria <mar.s.lebedeva@gmail.com>
2024-09-17 16:26:40 +0300

    refactor: потытка сделать в меньшее количество пробегов

--- lab2/cat.c
+++ lab2/cat.c
@@ -13,71 +13,71 @@ int lenInt(int num){
     }
     return len;
 }
-int sizeCount (char* filename, bool flagB, bool flagN){
-    int count = 1;
+void sizeCount(bool* beforLineHaveEnter, int* count, char* filename, bool flagB, bool flagN){
     FILE* file = fopen(filename, "r");
     if (!file && filename != NULL){
         fprintf(stderr, "./mycat: %s: No such file or directory\n", filename);
-        return -1;
+        *count = -1;
+        return;
     }
     char *line = NULL;
     size_t len = 0;
     ssize_t read;
-    while ((read = getline(&line, &len, file)) != -1) {
-        if (flagB && strcmp(line, "")){
-            count++;
+    while ((read = getline(&line, &len, file)) != -1){
+        if (flagB && strcmp(line, "") && *beforLineHaveEnter){
+            (*count)++;
         }
-        if (flagN && !flagB){
-            count++;
+        if (flagN && !flagB && *beforLineHaveEnter){
+            (*count)++;
+        }
+        if (line[strlen(line) - 1] != '\n'){
+            *beforLineHaveEnter = false;
+        }
+        else{
+            *beforLineHaveEnter = true;
         }
     }
     free(line);
     fclose(file);
-    return lenInt(count);
 }
 
-int outLog (char* filename, int sizeCount, bool flagB, bool flagN, bool flagE){
-    FILE* file = fopen(filename, "r");
-    if (!file && filename != NULL){
-        fprintf(stderr, "./mycat: %s: No such file or directory\n", filename);
-        return -1;
+void outLog (char* str, int* count, int sizeCount, bool flagB, bool flagN, bool flagE){
+    //FILE* file = fopen(filename, "r");
+    //if (!file && filename != NULL){
+    //    fprintf(stderr, "./mycat: %s: No such file or directory\n", filename);
+    //    return -1;
+    //}
+    //char *line = NULL;
+    //size_t len = 0;
+    //ssize_t read;
+    //int count = 1;
+    bool lastLine = true;
+    if (str[strlen(str) - 1] == '\n'){
+        str[strlen(str) - 1] = '\0';
+        lastLine = false;
     }
-    char *line = NULL;
-    size_t len = 0;
-    ssize_t read;
-    int count = 1;
-    while ((read = getline(&line, &len, file)) != -1) {
-        bool lastLine = true;
-        if (line[strlen(line) - 1] == '\n'){
-            line[strlen(line) - 1] = '\0';
-            lastLine = false;
-        }
-        if (flagB && strcmp(line, "")){
-            printf("    %*i  ", sizeCount, count);
-            count++;
-        }
-        if (flagN && !flagB){
-            printf("    %*i  ", sizeCount, count);
-            count++;
-        }
-        printf("%s", line);
-        if (flagE){
-            printf("$");
-        }
-        if (!lastLine){
-            printf("\n");
-        }
+    if (flagB && strcmp(str, "")){
+        printf("    %*i  ", sizeCount, *count);
+        count++;
     }
-    free(line);
-    fclose(file);
-    return 0;
+    if (flagN && !flagB){
+        printf("    %*i  ", sizeCount, *count);
+        count++;
+    }
+    printf("%s", str);
+    if (flagE){
+        printf("$");
+    }
+    if (!lastLine){
+        printf("\n");
+    }
 }
 
 int main(int argc, char** argv){
     int rez = 0;
     bool flagN = false;
     bool flagB = false;
-    bool flagE = false;
+    //bool flagE = false;
 	while ((rez = getopt(argc, argv, "nbE")) != -1){
 		switch (rez) {
 		case 'n': { //нумерует все строки выходного файла, начиная с 1 
@@ -89,7 +89,7 @@ int main(int argc, char** argv){
             break;
         }
         case 'E': { //выводит в конце каждой строки символ $ 
-            flagE = true;
+            //flagE = true;
             break;
         }
 		case '?': {
@@ -98,23 +98,48 @@ int main(int argc, char** argv){
         }
 		} 
 	}
-    char* filename = NULL;
+
+    char** files = calloc(1, sizeof(char*));
     int countFile = 0;
-    int firstFile = optind;
-    while (optind < argc) {
-        filename = calloc(strlen(argv[optind]) + 1, sizeof(char));
-        strcpy(filename, argv[optind]);
+    while (optind < argc){ // подсчет и сохраниение всех файлов для чтения
+        files[countFile] = calloc(strlen(argv[optind]) + 1, sizeof(char));
+        printf("%s\n", argv[optind]);
+        strcpy(files[countFile], argv[optind]);
+        printf("%s\n", files[countFile]);
         countFile++;
-        int size = sizeCount(filename, flagB, flagN);
-        if (size == -1) {
-            return 1;
-        }
-        outLog(filename, size, flagB, flagN, flagE);
         optind++;
     }
-    if (countFile == 0){
+
+    bool beforLineHaveEnter = true;
+    int countLine = 0;
+    for (int i = 0; i < countFile; i++){
+        sizeCount(&beforLineHaveEnter, &countLine, files[i], flagB, flagN);
+        printf("%i\n", countLine);
+    }
+
+
+
+    //while (optind < argc) {
+    //    filename = calloc(strlen(argv[optind]) + 1, sizeof(char));
+    //    strcpy(filename, argv[optind]);
+    //    haveFile = true;
+    //    sizeCount(&realCountLine, &countLineWithFlag, filename, flagB, flagN);
+    //    if (countLineWithFlag == -1) {
+    //        return 1;
+    //    }
+    //    optind++;
+    //}
+    if (countFile != 0){
+        //outLog(files[0], lenInt(countLine), flagB, flagN, flagE);
+    }
+    else{
         puts("-- no --");
     }
-    free(filename);
+
+    for (int i = 0; i < countFile; i++){
+        free(files[i]);
+    }
+    free(files);
+
     return 0;
 }
